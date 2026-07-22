90ad15cbb6ee1f80cc753b65584e956470a1160f
JoaquinRios17 <joaquinrioslukaku@gmail.com>
2026-07-21 21:00:29 -0500

    Update user.model.js

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,34 +1,32 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-    nombre: {
-        type: String,
-        required: true
-    },
+const UserSchema = new mongoose.Schema(
+  {
+    nombre: { type: String, required: true },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
-    password: {
-        type: String,
-        required: true
-    },
+    password: { type: String, required: true },
     rol: {
-        type: String,
-        enum: ["administrador", "docente", "estudiante"],
-        default: "estudiante"
-    }
+      type: String,
+      enum: ["administrador", "docente", "estudiante"],
+      default: "estudiante",
+    },
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const passwordEncriptada = await bcrypt.hash(this.password, 10);
+  this.password = passwordEncriptada;
+
+  next();
 });
 
-// antes de guardar, encriptamos la contraseña
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-});
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", UserSchema);
