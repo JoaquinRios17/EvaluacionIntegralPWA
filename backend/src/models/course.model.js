bc9fc550cb7fc1aa1800df7e85bd297d114d03c1
JoaquinRios17 <joaquinrioslukaku@gmail.com>
2026-07-21 20:59:54 -0500

    Update course.model.js

--- backend/src/models/course.model.js
+++ backend/src/models/course.model.js
@@ -1,29 +1,32 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const courseSchema = new mongoose.Schema({
+const CourseSchema = new Schema(
+  {
     nombre: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     descripcion: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     creditos: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     cupoMaximo: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     estado: {
-        type: String,
-        enum: ["activo", "inactivo"],
-        default: "activo"
-    }
-}, { timestamps: true });
+      type: String,
+      enum: ["activo", "inactivo"],
+      default: "activo",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Course = mongoose.model("Course", courseSchema);
-
-module.exports = Course;
+module.exports = model("Course", CourseSchema);
